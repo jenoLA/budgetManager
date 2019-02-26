1c27f132ede8fd879fea496517918b920dfdfe8a
João Marcos <marcospb19@hotmail.com>
2019-02-26 16:43:49 -0300

    Making indentation constant
    From mixed spaces and tabs indentation
    to only tabs indentation

--- src/main.c
+++ src/main.c
@@ -12,41 +12,41 @@ int main()
 
 	while (1) {
 
-	    printf("by now we have the following functionalities\n");
-		printf("Q)uit  L)ist budgets  N)ew budget*   E)nter budget   D)elete budget\n");
+		printf("by now we have the following functionalities\n");
+		printf("Q)uit, L)ist budgets, N)ew budget*,  E)nter budget,  D)elete budget\n");
 
-	    scanf(" %c", &option);
+		scanf(" %c", &option);
 
-	    if (option >= 65 && option <= 90)
-	    {
-	        option += 32;
-	    }
-	    if (option == 'q')
-	    {
-	        break;
-	    }
-	    else if (option == 'l')
-	    {
-	     //   printBudgets(list);
-	    }
-	    else if (option == 'n')
-	    {
-	        // printf("name to the new budget of twenty caracters or least:\n");
-	        // scanf(" %s", &name);
-	        // newBudget(list, name);
-	        // printf("done!\n\n");
-	    }
-	    else if (option == 'd')
-	    {
-	        // printf("name of the budget to delete:\n");
-	        // scanf(" %s", &name);
-	        // deleteBudget(list, name);
-	        // printf("done!\n\n");
-	    }
-	    else if (option == 'e')
-	    {
-		//	paperLoop();
-	    }
+		if (option >= 65 && option <= 90)
+		{
+			option += 32;
+		}
+		if (option == 'q')
+		{
+			break;
+		}
+		else if (option == 'l')
+		{
+			// printBudgets(list);
+		}
+		else if (option == 'n')
+		{
+			// printf("name to the new budget of twenty caracters or least:\n");
+			// scanf(" %s", &name);
+			// newBudget(list, name);
+			// printf("done!\n\n");
+		}
+		else if (option == 'd')
+		{
+			// printf("name of the budget to delete:\n");
+			// scanf(" %s", &name);
+			// deleteBudget(list, name);
+			// printf("done!\n\n");
+		}
+		else if (option == 'e')
+		{
+			// paperLoop();
+		}
 	}
 
 	return 0;
