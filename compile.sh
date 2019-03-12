9d3237b4b7e8f0cc5ab3f72672a4afa6ff044c90
jeno <jenoandrade@gmail.com>
2019-03-12 09:47:00 -0300

    minor changes and adding folder to save test files

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
 # Using Gnu Compiler Collection
-gcc -Wall src/* -o budget-manager
+gcc -Wall src/* -o budget-manager && ./budget-manager
 
 # Using Tiny C Compiler
-# tcc src/* -o budget-manager
+# tcc src/* -o budget-manager && ./budget-manager
 
 # Using Musl wrapper to GCC
-# musl-gcc src/* -o budget-manager
+# musl-gcc src/* -o budget-manager && ./budget-manager
+
+
