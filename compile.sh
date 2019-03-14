708f2f9b2c453e5d0b9dc996c2b5cb7d151291b1
João Marcos <marcospb19@hotmail.com>
2019-03-14 17:05:31 -0300

    Added run.sh, compile.sh now only compiles

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 # Using Gnu Compiler Collection
-gcc -Wall src/* -o budget-manager && ./budget-manager
+gcc -Wall src/* -o budget-manager
 
 # Using Tiny C Compiler
-# tcc src/* -o budget-manager && ./budget-manager
+# tcc src/* -o budget-manager
 
 # Using Musl wrapper to GCC
-# musl-gcc src/* -o budget-manager && ./budget-manager
+# musl-gcc src/* -o budget-manager
 
 
---
+++ run.sh
@@ -0,0 +1,10 @@
+# Using Gnu Compiler Collection
+gcc -Wall src/* -o budget-manager && ./budget-manager
+
+# Using Tiny C Compiler
+# tcc src/* -o budget-manager && ./budget-manager
+
+# Using Musl wrapper to GCC
+# musl-gcc src/* -o budget-manager && ./budget-manager
+
+
